3ce61af67f8d2b4d9c98d2a453b2519df8910107
Makaratel <pmakaewskij@mail.ru>
2019-09-29 11:39:03 +0300

    Part four

--- source/js/script.js
+++ source/js/script.js
@@ -21,36 +21,50 @@ var decreaseCounter = function () {
   frontCounter = (frontCounter < 1) ? 3 : frontCounter;
 };
 
-var animate = function (element, coefficient1, coefficient2) {
+var animate = function (slider, coefficient1, coefficient2) {
   var start = performance.now();
 
   requestAnimationFrame (function animate (time) {
     var timeFraction = (time - start) / 500;
-    if (timeFraction > 1) {
-      timeFraction = 1;
-      sliderFront.src = 'img/slide-' + frontCounter + '.jpg';
-      sliderBack.src = 'img/slide-' + backCounter + '.jpg';
-    }
-
-    var progress = Math.pow(timeFraction, 3);
-    element.style.left = coefficient1 + progress * coefficient2 + '%';
+    var progress = Math.pow(timeFraction, 2);
 
     if (timeFraction < 1) {
       requestAnimationFrame(animate);
+    } else if (timeFraction > 1) {
+      timeFraction = 1;
     }
+
+    slider.style.left = (progress - coefficient1) * coefficient2 + '%';
   });
 };
 
+var changePhoto = function (coefficient) {
+  animate(sliderFront, 0, coefficient);
+  setTimeout(function () {
+    sliderFront.src = 'img/slide-' + frontCounter + '.jpg';
+    animate(sliderBack, 0, coefficient);
+    animate(sliderFront, 1, coefficient);
+
+    setTimeout(function () {
+      sliderFront.style.left = 0 + '%';
+      sliderBack.style.left = -coefficient + '%';
+      sliderBack.src = 'img/slide-' + backCounter + '.jpg';
+      animate(sliderBack, 1, coefficient);
+    }, 600);
+  }, 600);
+
+  setTimeout(function () {
+    sliderBack.style.left = 0 + '%';
+  },1750);
+
+}
+
 arrowLeft.addEventListener('click', function () {
   increaseCounter();
-  animate(sliderFront);
+  changePhoto(-100);
 });
 
 arrowRight.addEventListener('click', function () {
   decreaseCounter();
-  animate(sliderFront, 0, 100);
-  animate(sliderFront, -100, 100);
+  changePhoto(100);
 });
-
-
-
